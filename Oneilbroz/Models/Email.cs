665bffe6c2c66fee63fe3c2ac766525f795294a7
Adam O'Neil <adamosoftware@gmail.com>
2022-10-22 15:51:06 -0400

    added email model and submit form

---
+++ Oneilbroz/Models/Email.cs
@@ -0,0 +1,10 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Oneilbroz.Models
+{
+    public class Email
+    {
+        [Required(AllowEmptyStrings = false, ErrorMessage = "Email is required")]
+        public string Value { get; set; } = string.Empty;
+    }
+}
---
+++ Oneilbroz/Models/Item.cs
@@ -0,0 +1,11 @@
+﻿namespace Oneilbroz.Models
+{
+    public class Item
+    {
+        public string Id { get; set; } = string.Empty;
+        public string Name { get; set; } = string.Empty;
+        public string Description { get; set; } = string.Empty;
+        public string Thumbnail { get; set; } = "thumb.png";
+        public decimal Price { get; set; }
+    }
+}
